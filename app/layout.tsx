d6657d43929ffd1ab4127e9210a4ac0b3a81674b
Reddy Saneesh Sumesh <163725665+2300031420@users.noreply.github.com>
2025-09-07 19:29:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistSans = GeistSans.variable
 const geistMono = GeistMono.variable
 
 export const metadata: Metadata = {
-  title: "TEACHER PORTAL",
+  title: "Teacher-Portal",
   description: "Created with v0",
   generator: "v0.app",
   viewport: {
